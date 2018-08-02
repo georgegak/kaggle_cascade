f4ef3553007667f75dc4fcc32a2e74117da51430
stormrage <gaboukassm@gmail.com>
2018-08-03 00:11:39 +0300

    version 3

--- extract.py
+++ extract.py
@@ -3,18 +3,24 @@ import os
 import pickle
 
 # GLOBALS
-data_directory = "D:\\Projects\\kaggle_cascade\\Data\\"
+data_directory = "/home/stormrage/Projects/kaggle_cascade/Data/"
 image_width = 768
 image_height = 768
 
 
 def get_image_data():
-
     images = []
+    images_data=[]
     minimized_index = 0
     current_index = 0
-    for filename in os.listdir(data_directory + "train\\"):
+    list_os=os.listdir(data_directory + "train")
+    list_os.sort()
+    for filename in list_os:
+        with open(data_directory+"train/"+filename, "rb") as imgText:
+            tmp_img=imgText.read()
+            b = bytearray(tmp_img,'base64')
         images.append([filename, []])
+        images_data.append([filename,b])
         current_index += 1
         if current_index == minimized_index and minimized_index != 0:
             break
@@ -36,28 +42,35 @@ def get_image_data():
             break
     with open(data_directory + 'hitmask', 'wb') as fp:
         pickle.dump(images, fp)
+    with open(data_directory + 'bundled_images', 'wb') as fp:
+        pickle.dump(images_data, fp)
     return images
 
 
 def extract_to_cnn(mask_size, step_size, reload=False):
     output_cnn = []
-    if reload:
+    if reload or not os.path.exists(data_directory + 'hitmask'):
         images = get_image_data()
     else:
         with open(data_directory + 'hitmask', 'rb') as fp:
             images = pickle.load(fp)
-    for i in images:
-        if len(i[1]) == 0:
-            zero_out = True
-        else:
-            zero_out = False
-        temp = []
-        for y in range(0, image_height - mask_size, step_size):
-            for x in range(0, image_width - mask_size, step_size):
-                if (x * (y + 1)) in i[1]:
-                    temp.append(1)
-                else:
-                    temp.append(0)
-        output_cnn.append(temp)
-    with open(data_directory + 'masks\mask_' + str(mask_size) + '_' + str(step_size), 'wb') as fp:
-        pickle.dump(output_cnn, fp)
+    if reload or not os.path.exists(data_directory + 'masks/mask_' + str(mask_size) + '_' + str(step_size)):
+        for i in images:
+            if len(i[1]) == 0:
+                zero_out = True
+            else:
+                zero_out = False
+            temp = []
+            for y in range(0, image_height - mask_size, step_size):
+                for x in range(0, image_width - mask_size, step_size):
+                    if zero_out or (x * (y + 1)) not in i[1]:
+                        temp.append(0)
+                    else:
+                        temp.append(1)
+            output_cnn.append([i[0],temp])
+        with open(data_directory + 'masks/mask_' + str(mask_size) + '_' + str(step_size), 'wb') as fp:
+            pickle.dump(output_cnn, fp)
+    else:
+        with open(data_directory + 'masks/mask_' + str(mask_size) + '_' + str(step_size), 'rb') as fp:
+            output_cnn = pickle.load(fp)
+    return output_cnn
--- main.py
+++ main.py
@@ -1,4 +1,48 @@
-from extract import get_image_data
+#Import
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras.layers.core import Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+from keras import backend as K
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from sklearn.utils import shuffle
+import sklearn.preprocessing as pre
+import os
+import sys
+import cv2
+import numpy as np
+import h5py
+import time
+import math
+import copy
+from extract import extract_to_cnn
 
+#Globals
+seed = 50
+np.random.seed(seed)
+opt = SGD(lr=0.1)
+training=False
+resume_training=False
+skip_training_first=False
 
-result =get_image_data()
+def initialize_model(weight):
+    #Cascade
+    early_stopping = EarlyStopping(monitor='loss', patience=15)
+    save_best=ModelCheckpoint('weights'+weight, monitor='loss', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
+    np.random.seed(seed)
+    #Model1
+    model1 = Sequential()
+    model1.add(Convolution2D(64, 5, 5, border_mode="same",input_shape=(3, 768, 768),bias=True,init='he_normal'))
+    model1.add(Activation("relu"))
+    model1.add(MaxPooling2D(pool_size=(2, 2)))
+    model1.add(Flatten())
+    model1.add(Dense(21,bias=True,init='glorot_normal'))
+    return model1
+
+nol=extract_to_cnn(10,10,True)
